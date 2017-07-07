e52d2b71ddbd187d37b4112a0f0d345b654ff199
Jeremie CUADRADO <jcuadrado@redbeard-consulting.fr>
2017-07-07 09:37:24 +0000

    Correction bug

--- roles/docker-spacewalk/files/spacewalk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if ! [ -f "/opt/.spacewalk_initialized" ]; then
-    spacewalk-setup --non-interactive --answer-file=/opt/answer.txt
-    sleep 5
-    touch /opt/.spacewalk_initialized
-fi
-spacewalk-service start
-exit 0
